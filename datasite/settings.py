1db2f8fde01d6a4361f298e61cd4f6bbd12d4bd9
wikiselev <vlad.kiselev@alethiomics.com>
2022-07-01 15:36:51 +0100

    working - sending json via api to frontend

--- datasite/settings.py
+++ datasite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'ggplotapp',
     'rest_framework'
 ]
@@ -49,12 +50,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'datasite.urls'
 
--- ggplotapp/views.py
+++ ggplotapp/views.py
@@ -1,39 +1,38 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
 from matplotlib import pyplot as plt
 import io
 import base64
 from django.conf import settings
 from .forms import NameForm
+from .serializers import ImageSerializer
+from rest_framework.renderers import JSONRenderer
 
-from plotnine import ggplot, geom_point, aes, stat_smooth, facet_wrap, ggsave
+from plotnine import ggplot, geom_point, aes, stat_smooth, facet_wrap, ggtitle
 from plotnine.data import mtcars
 
 plt.switch_backend('Agg') 
 
+class Plot:
+    def __init__(self, image):
+        self.image = image
+
+@api_view()
 def index(request):
-
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            p = (
-                ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)')) 
-                + geom_point()
-                + stat_smooth(method='lm')
-                + facet_wrap('~gear')
-            )
+    if request.method == 'GET':
+        p = (
+            ggplot(mtcars, aes('wt', 'mpg', color='factor(gear)')) 
+            + geom_point()
+            + stat_smooth(method='lm')
+            + facet_wrap('~gear')
+            + ggtitle("test")
+        )
+        sio = io.BytesIO()
+        p.save(sio, format="jpg")
+        encoded_img = base64.b64encode(sio.getvalue()).decode(settings.DEFAULT_CHARSET)
+        plot = Plot(image=encoded_img)
+        serializer = ImageSerializer(plot)
+        # serializer.image = encoded_img
+        return JsonResponse(serializer.data)
 
-            sio = io.BytesIO()
-            # plt.savefig(sio, format="png")
-            p.save(sio, format="png")
-            encoded_img = base64.b64encode(sio.getvalue()).decode(settings.DEFAULT_CHARSET)
-            return render(request, 'ggplotapp/index.html', {'form': form, 'image' : encoded_img, 'post' : 1})
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'ggplotapp/index.html', {'form': form, 'post' : 0})
